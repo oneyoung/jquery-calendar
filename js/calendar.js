7fd398223b1aa6ef8b7f0f76571f3542b3807c37
Wayne <guowangyang@gmail.com>
2013-07-24 15:21:54 +0800

    + js/calendar.js: init function

--- js/calendar.js
+++ js/calendar.js
@@ -1,7 +1,33 @@
 
 (function ($) {
 	$.fn.calendar = function () {
+		var _this = this;
+		var week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
+		_this.init = function () {
+			var tpl = '<table class="cal">' +
+			'<caption>' +
+			'	<span class="prev"><a href="#">&larr;</a></span>' +
+			'	<span class="next"><a href="#">&rarr;</a></span>' +
+			'	<span class="month"><span>' +
+			"</caption>" +
+			"<thead>" +
+			"	<tr>" +
+			"	</tr>" +
+			"</thead>" +
+			"<tbody>" +
+			"</tbody>" + "</table>";
+			var html = $(tpl);
+			/* add week header -- Mon, Tue, Wed, etc */
+			var tHead = $(html).find('thead tr');
+			week.forEach(function (name) {
+				tHead.append($("<th>" + name + "</th>"));
+			});
+			_this.append(html);
+		};
+
+		_this.init();
+		return this;
 	};
 }($));
 
