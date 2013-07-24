ed97f36096cac6beb39ba843d227a50d855584d2
Wayne <guowangyang@gmail.com>
2013-07-24 17:37:58 +0800

    * js/calendar.js: init less jQuery

--- js/calendar.js
+++ js/calendar.js
@@ -3,6 +3,9 @@
 	$.fn.calendar = function () {
 		var _this = this;
 		var week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+		var tHead = week.map(function (day) {
+			return "<th>" + day + "</th>";
+		}).join("");
 
 		_this.init = function () {
 			var tpl = '<table class="cal">' +
@@ -11,18 +14,12 @@
 			'	<span class="next"><a href="#">&rarr;</a></span>' +
 			'	<span class="month"><span>' +
 			"</caption>" +
-			"<thead>" +
-			"	<tr>" +
-			"	</tr>" +
-			"</thead>" +
+			"<thead><tr>" +
+			tHead +
+			"</tr></thead>" +
 			"<tbody>" +
 			"</tbody>" + "</table>";
 			var html = $(tpl);
-			/* add week header -- Mon, Tue, Wed, etc */
-			var tHead = $(html).find('thead tr');
-			week.forEach(function (name) {
-				tHead.append($("<th>" + name + "</th>"));
-			});
 			_this.append(html);
 		};
 
